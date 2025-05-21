ba95a9bb6cd382947b04d3b622b67c828916c4c8
Mahmoud Nasser <109390357+MahmoudNasser127@users.noreply.github.com>
2025-05-21 09:27:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from transformers import Wav2Vec2FeatureExtractor
 
 @st.cache_resource(show_spinner=False)
 def load_models():
-    model_name = "ylacombe/accent-classifier"  # مسار الموديل المحلي
+    model_name = "dima806/multiple_accent_classification"
     feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
     model = AutoModelForAudioClassification.from_pretrained(model_name)
     return feature_extractor, model
